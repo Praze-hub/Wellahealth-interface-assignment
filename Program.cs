747c27be6108f4d1a9475939560205f5ca6cdeb9
Praze-hub <godspraiseokechukwu07@gmail.com>
2022-06-15 23:38:56 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Assignment
+{
+    public interface IVehicle
+    {
+        void Drive();
+        bool Refuel(int a);
+    }
+        public class Car : IVehicle{
+            public int Gas
+            {get; set;}
+
+            public Car(int gas)
+            {
+                Gas = gas;
+                
+            }
+            public void Drive(){
+                if(Gas > 0)
+                {
+                    Console.WriteLine("Car is driving");
+                }else{
+                    Console.WriteLine("There is no fuel");
+                }
+            }
+            public bool Refuel(int a){
+                Gas +=a;
+                return true;
+            }
+        }
+        public class Program{
+
+       
+        static void Main(string[] args)
+        {
+            Car benz = new Car(0);
+            Console.WriteLine("How much of gas do you want:");
+            int gas = int.Parse(Console.ReadLine());
+
+            if(benz.Refuel(gas)){
+                benz.Drive();
+            }
+        }
+    }
+}
+ 
